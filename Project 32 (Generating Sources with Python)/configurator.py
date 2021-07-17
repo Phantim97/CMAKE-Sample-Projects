1171ae1fc658251ebc827f4a7fe7a988354d02cb
Tim Coelho <timothycoelho@gmail.com>
2021-07-17 18:55:46 -0400

    Python Code Generation example added.

---
+++ Project 32 (Generating Sources with Python)/configurator.py
@@ -0,0 +1,9 @@
+def configure_file(input_file, output_file, vars_dict):
+        with input_file.open('r') as f:
+            template = f.read()
+
+        for vars in vars_dict:
+            template = template.replace('@' + var + '@', vars_dict[var])
+
+        with output_file.open('w') as f:
+            f.write(template)
